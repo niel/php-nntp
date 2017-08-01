bc14cd11e63f48c0fd3c5a27b39ccd47c0b0fd8d
Heino H. Gehlsen <heino@gehlsen.dk>
2017-08-01 15:49:05 +0200

    Enclose constants within new class; Remove constant prefix

--- NNTP/Protocol/Responsecode.php
+++ NNTP/Protocol/Responsecode.php
@@ -64,6 +64,17 @@
  * @since      File available since release 1.3.0
  */
 
+/**
+ * 
+ *
+ * @package    Net_NNTP
+ * @author     Heino H. Gehlsen <heino@gehlsen.dk>
+ * @version    package: @package_version@ (@package_state@)
+ * @internal
+ * @see        Net_NNTP_Protocol_Client
+ */
+class Net_NNTP_Protocol_Responsecode
+{
 
 /* Constants: Connection */
 
@@ -72,14 +83,14 @@
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_READY_POSTING_ALLOWED', 200);
+const READY_POSTING_ALLOWED = 200;
 
 /**
  * 'Server ready - no posting allowed' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_READY_POSTING_PROHIBITED', 201);
+const READY_POSTING_PROHIBITED = 201;
 
 
 /**
@@ -88,7 +99,7 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_READY_POSTING_PROHIBITED', 201);
  * @access     public
  * @since      ?
  */
-//define('NET_NNTP_PROTOCOL_RESPONSECODE_DISCONNECTING_REQUESTED', 205);   ///// goodbye
+//const DISCONNECTING_REQUESTED = 205;   ///// goodbye
 
 /**
  * 'Service discontinued' (RFC977)
@@ -96,7 +107,7 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_READY_POSTING_PROHIBITED', 201);
  * @access     public
  * @since      ?
  */
-//define('NET_NNTP_PROTOCOL_RESPONSECODE_DISCONNECTING_FORCED', 400);   ///// unavailable / discontinued
+//const DISCONNECTING_FORCED = 400;   ///// unavailable / discontinued
 
 
 /**
@@ -104,7 +115,7 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_READY_POSTING_PROHIBITED', 201);
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_SLAVE_RECOGNIZED', 202);
+const SLAVE_RECOGNIZED = 202;
 
 
 
@@ -118,28 +129,28 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_SLAVE_RECOGNIZED', 202);
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_UNKNOWN_COMMAND', 500);
+const UNKNOWN_COMMAND = 500;
 
 /**
  * 'Command syntax error' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_SYNTAX_ERROR', 501);
+const SYNTAX_ERROR = 501;
 
 /**
  * 'Access restriction or permission denied' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_NOT_PERMITTED', 502);
+const NOT_PERMITTED = 502;
 
 /**
  * 'Program fault - command not performed' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_NOT_SUPPORTED', 503);
+const NOT_SUPPORTED = 503;
 
 
 
@@ -152,14 +163,14 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_NOT_SUPPORTED', 503);
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_GROUP_SELECTED', 211);
+const GROUP_SELECTED = 211;
 
 /**
  * 'No such news group' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_NO_SUCH_GROUP', 411);
+const NO_SUCH_GROUP = 411;
 
 
 
@@ -172,28 +183,28 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_NO_SUCH_GROUP', 411);
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_ARTICLE_FOLLOWS', 220);
+const ARTICLE_FOLLOWS = 220;
 
 /**
  * 'Article retrieved - head follows' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_HEAD_FOLLOWS', 221);
+const HEAD_FOLLOWS = 221;
 
 /**
  * 'Article retrieved - body follows' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_BODY_FOLLOWS', 222);
+const BODY_FOLLOWS = 222;
 
 /**
  * 'Article retrieved - request text separately' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_ARTICLE_SELECTED', 223);
+const ARTICLE_SELECTED = 223;
 
 
 
@@ -204,7 +215,7 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_ARTICLE_SELECTED', 223);
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_NO_GROUP_SELECTED', 412);
+const NO_GROUP_SELECTED = 412;
 
 
 /**
@@ -212,21 +223,21 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_NO_GROUP_SELECTED', 412);
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_NO_ARTICLE_SELECTED', 420);
+const NO_ARTICLE_SELECTED = 420;
 
 /**
  * 'No next article in this group' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_NO_NEXT_ARTICLE', 421);
+const NO_NEXT_ARTICLE = 421;
 
 /**
  * 'No previous article in this group' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_NO_PREVIOUS_ARTICLE', 422);
+const NO_PREVIOUS_ARTICLE = 422;
 
 
 /**
@@ -234,15 +245,14 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_NO_PREVIOUS_ARTICLE', 422);
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_NO_SUCH_ARTICLE_NUMBER', 423);
+const NO_SUCH_ARTICLE_NUMBER = 423;
 
 /**
  * 'No such article found' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_NO_SUCH_ARTICLE_ID', 430);
-
+const NO_SUCH_ARTICLE_ID = 430;
 
 
 
@@ -256,35 +266,35 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_NO_SUCH_ARTICLE_ID', 430);
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_TRANSFER_SEND', 335);
+const TRANSFER_SEND = 335;
 
 /**
  * 'Article transferred ok' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_TRANSFER_SUCCESS', 235);
+const TRANSFER_SUCCESS = 235;
 
 /**
  * 'Article not wanted - do not send it' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_TRANSFER_UNWANTED', 435);
+const TRANSFER_UNWANTED = 435;
 
 /**
  * 'Transfer failed - try again later' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_TRANSFER_FAILURE', 436);
+const TRANSFER_FAILURE = 436;
 
 /**
  * 'Article rejected - do not try again' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_TRANSFER_REJECTED', 437);
+const TRANSFER_REJECTED = 437;
 
 
 
@@ -297,28 +307,28 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_TRANSFER_REJECTED', 437);
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_POSTING_SEND', 340);
+const POSTING_SEND = 340;
 
 /**
  * 'Article posted ok' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_POSTING_SUCCESS', 240);
+const POSTING_SUCCESS = 240;
 
 /**
  * 'Posting not allowed' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_POSTING_PROHIBITED', 440);
+const POSTING_PROHIBITED = 440;
 
 /**
  * 'Posting failed' (RFC977)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_POSTING_FAILURE', 441);
+const POSTING_FAILURE = 441;
 
 
 
@@ -332,28 +342,28 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_POSTING_FAILURE', 441);
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_AUTHORIZATION_REQUIRED', 450);
+const AUTHORIZATION_REQUIRED = 450;
 
 /**
  * 'Continue with authorization sequence' (RFC2980)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_AUTHORIZATION_CONTINUE', 350);
+const AUTHORIZATION_CONTINUE = 350;
 
 /**
  * 'Authorization accepted' (RFC2980)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_AUTHORIZATION_ACCEPTED', 250);
+const AUTHORIZATION_ACCEPTED = 250;
 
 /**
  * 'Authorization rejected' (RFC2980)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_AUTHORIZATION_REJECTED', 452);
+const AUTHORIZATION_REJECTED = 452;
 
 
 
@@ -367,28 +377,28 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_AUTHORIZATION_REJECTED', 452);
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_AUTHENTICATION_REQUIRED', 480);
+const AUTHENTICATION_REQUIRED = 480;
 
 /**
  * 'More authentication information required' (RFC2980)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_AUTHENTICATION_CONTINUE', 381);
+const AUTHENTICATION_CONTINUE = 381;
 
 /**
  * 'Authentication accepted' (RFC2980)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_AUTHENTICATION_ACCEPTED', 281);
+const AUTHENTICATION_ACCEPTED = 281;
 
 /**
  * 'Authentication rejected' (RFC2980)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_AUTHENTICATION_REJECTED', 482);
+const AUTHENTICATION_REJECTED = 482;
     
 
 
@@ -401,70 +411,70 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_AUTHENTICATION_REJECTED', 482);
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_HELP_FOLLOWS', 100);
+const HELP_FOLLOWS = 100;
 
 /**
  * 'Capabilities list follows' (Draft)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_CAPABILITIES_FOLLOW', 101);
+const CAPABILITIES_FOLLOW = 101;
 
 /**
  * 'Server date and time' (Draft)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_SERVER_DATE', 111);
+const SERVER_DATE = 111;
 
 /**
  * 'Information follows' (Draft)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_GROUPS_FOLLOW', 215);
+const GROUPS_FOLLOW = 215;
 
 /**
  * 'Overview information follows' (Draft)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_OVERVIEW_FOLLOWS', 224);
+const OVERVIEW_FOLLOWS = 224;
 
 /**
  * 'Headers follow' (Draft)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_HEADERS_FOLLOW', 225);
+const HEADERS_FOLLOW = 225;
 
 /**
  * 'List of new articles follows' (Draft)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_NEW_ARTICLES_FOLLOW', 230);
+const NEW_ARTICLES_FOLLOW = 230;
 
 /**
  * 'List of new newsgroups follows' (Draft)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_NEW_GROUPS_FOLLOW', 231);
+const NEW_GROUPS_FOLLOW = 231;
 
 /**
  * 'The server is in the wrong mode; the indicated capability should be used to change the mode' (Draft)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_WRONG_MODE', 401);
+const WRONG_MODE = 401;
 
 /**
  * 'Internal fault or problem preventing action being taken' (Draft)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_INTERNAL_FAULT', 403);
+const INTERNAL_FAULT = 403;
 
 /**
  * 'Command unavailable until suitable privacy has been arranged' (Draft)
@@ -475,13 +485,13 @@ define('NET_NNTP_PROTOCOL_RESPONSECODE_INTERNAL_FAULT', 403);
  * @access     public
  * @since      ?
  */
-//define('NET_NNTP_PROTOCOL_RESPONSECODE_ENCRYPTION_REQUIRED', 483);
+//const ENCRYPTION_REQUIRED = 483;
 
 /**
  * 'Error in base64-encoding [RFC3548] of an argument' (Draft)
  *
  * @access     public
  */
-define('NET_NNTP_PROTOCOL_RESPONSECODE_BASE64_ENCODING_ERROR', 504);
+const BASE64_ENCODING_ERROR = 504;
 
-
+}
